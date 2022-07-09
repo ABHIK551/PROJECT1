2544c80e926abe403315cdb1d8c4979ecd074948
ABHIK551 <108769917+ABHIK551@users.noreply.github.com>
2022-07-09 13:48:26 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,104 +1,142 @@
+<?php 
+session_start();
+error_reporting(0);
+include 'include/config.php';
+$uid=$_SESSION['uid'];
 
+if(isset($_POST['submit']))
+{ 
+$pid=$_POST['pid'];
+
+
+$sql="INSERT INTO tblbooking (package_id,userid) Values(:pid,:uid)";
+
+$query = $dbh -> prepare($sql);
+$query->bindParam(':pid',$pid,PDO::PARAM_STR);
+$query->bindParam(':uid',$uid,PDO::PARAM_STR);
+$query -> execute();
+echo "<script>alert('Package has been booked.');</script>";
+echo "<script>window.location.href='booking-history.php'</script>";
+
+}
+
+?>
 <!DOCTYPE html>
-<html>
+<html lang="zxx">
 <head>
-<title>Online DJ Management System||Home Page</title>
-<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
-<!-- jQuery (Bootstrap's JavaScript plugins) -->
-<script src="js/jquery.min.js"></script>
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!-- Custom Theme files -->
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<title>Gym Management System</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Ahana Yoga HTML Template">
+	<meta name="keywords" content="yoga, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="css/nice-select.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
 
-<link href='http://fonts.googleapis.com/css?family=Monoton' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
 </head>
 <body>
-<!---->
-<?php include_once('includes/header.php');?>
-<!---->
-<script src="js/responsiveslides.min.js"></script>
-  <script>
-    // You can also use "$(window).load(function() {"
-    $(function () {
-      $("#slider").responsiveSlides({
-      	auto: true,
-        manualControls: '#slider3-pager',
-      });
-    });
-  </script> 
-<div class="content">
-	 <div class="container">  
-		  <div class="slider">
-			<!-- Slideshow 3 -->
-			<ul class="rslides" id="slider">
-			  <li><img src="images/7.jpg" alt="">
-				<div class="caption">
-					<h1>Image1</h1>
-					<p>Nam est purus, egestas eu varius in, commodo vitae massa. Fusce mollis a lacus in tincidunt.</p>
-				</div>
-			  </li>
-			  <li><img src="images/2.jpg" alt="">
-				<div class="caption">
-					<h1>Image2</h1>	  
-					<p>Nam est purus, egestas eu varius in, commodo vitae massa. Fusce mollis a lacus in tincidunt.</p>
-				</div>
-			  </li>	      
-			  <li><img src="images/10.jpg" alt="">
-				<div class="caption">
-					<h1>Image3</h1>	  
-					<p>Nam est purus, egestas eu varius in, commodo vitae massa. Fusce mollis a lacus in tincidunt.</p>
-				</div>
-			  </li>
-			  <li><img src="images/5.jpg" alt="">
-				<div class="caption">
-					<h1>Image1</h1>
-					<p>Nam est purus, egestas eu varius in, commodo vitae massa. Fusce mollis a lacus in tincidunt.</p>
-				</div>
-			  </li>
-			  <li><img src="images/8.jpg" alt="">
-				<div class="caption">
-					<h1>Image2</h1>	  
-					<p>Nam est purus, egestas eu varius in, commodo vitae massa. Fusce mollis a lacus in tincidunt.</p>
-				</div>
-			  </li>	      
-			  <li><img src="images/1.jpg" alt="">
-				<div class="caption">
-					<h1>Image3</h1>	  
-					<p>Nam est purus, egestas eu varius in, commodo vitae massa. Fusce mollis a lacus in tincidunt.</p>
-				</div>
-			  </li>
-			  <li><img src="images/13.jpg" alt="">
-				<div class="caption">
-					<h1>Image2</h1>	  
-					<p>Nam est purus, egestas eu varius in, commodo vitae massa. Fusce mollis a lacus in tincidunt.</p>
-				</div>
-			  </li>	      
-			  <li><img src="images/3.jpg" alt="">
-				<div class="caption">
-					<h1>Image3</h1>	  
-					<p>Nam est purus, egestas eu varius in, commodo vitae massa. Fusce mollis a lacus in tincidunt.</p>
-				</div>
-			  </li>
-			</ul>
-			<!-- Slideshow 3 Pager --></br></br>
-			<ul id="slider3-pager">
-			  <li><a href="#"><img src="images/77.jpg" alt=""></a></li>
-			  <li><a href="#"><img src="images/22.jpg" alt=""></a></li>
-			  <li><a href="#"><img src="images/1010.jpg" alt=""></a></li>
-			   <li><a href="#"><img src="images/55.jpg" alt=""></a></li>
-			  <li><a href="#"><img src="images/88.jpg" alt=""></a></li>
-			  <li><a href="#"><img src="images/11.jpg" alt=""></a></li>
-			  <li><a href="#"><img src="images/1313.jpg" alt=""></a></li>
-			  <li><a href="#"><img src="images/33.jpg" alt=""></a></li>
-			</ul>   
-				<div class="clearfix"></div>
-		  </div>
-	 </div>
-	<?php include_once('includes/footer.php');?>
-</div>
-<!---->
+	<!-- Page Preloder -->
+	
 
-<!---->
-</body>
+	<!-- Header Section -->
+	<?php include 'include/header.php';?>
+	<!-- Header Section end -->
+
+	
+
+	                                                                              
+	<!-- Page top Section -->
+	<section class="page-top-section set-bg" data-setbg="img/page-top-bg.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7 m-auto text-white">
+					<h2>Home</h2>
+					<p>Physical Activity Or Can Improve Your Health</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+	<!-- Pricing Section -->
+	<section class="pricing-section spad">
+		<div class="container">
+			<div class="section-title text-center">
+				<img src="img/icons/logo-icon.png" alt="">
+				<h2>Pricing plans</h2>
+				<p>Practice Yoga to perfect physical beauty, take care of your soul and enjoy life more fully!</p>
+			</div>
+			<div class="row">
+				        <?php 
+
+$sql ="SELECT id, category, titlename, PackageType, PackageDuratiobn, Price, uploadphoto, Description, create_date from tbladdpackage";
+$query= $dbh -> prepare($sql);
+$query-> execute();
+$results = $query -> fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query -> rowCount() > 0)
+{
+foreach($results as $result)
+{
+?>
+				<div class="col-lg-3 col-sm-6">
+					<div class="pricing-item begginer">
+						<div class="pi-top">
+							<h4><?php echo $result->titlename;?></h4>
+						</div>
+						<div class="pi-price">
+							<h3><?php echo htmlentities($result->Price);?></h3>
+							<p>	<?php echo $result->PackageDuratiobn;?></p>
+						</div>
+						<ul>
+							<?php echo $result->Description;?>
+							
+						</ul>
+						<?php if(strlen($_SESSION['uid'])==0): ?>
+						<a href="login.php" class="site-btn sb-line-gradient">Booking Now</a>
+						<?php else :?>
+							<!-- <a href="#" class="site-btn sb-line-gradient">Booking Now</a> -->
+							 <form method='post'>
+                            <input type='hidden' name='pid' value='<?php echo htmlentities($result->id);?>'>
+                          
+
+                        <input class='site-btn sb-line-gradient' type='submit' name='submit' value='Booking Now' onclick="return confirm('Do you really want to book this package.');"> 
+                        </form> 
+							 <?php endif;?>
+					</div>
+				</div>
+				<?php  $cnt=$cnt+1; } } ?>
+			</div>
+		</div>
+	</section>
+	
+
+	<!-- Footer Section -->
+	<?php include 'include/footer.php'; ?>
+	<!-- Footer Section end -->
+
+	<div class="back-to-top"><img src="img/icons/up-arrow.png" alt=""></div>
+
+	<!-- Search model end -->
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/vendor/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.nice-select.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
 </html>
